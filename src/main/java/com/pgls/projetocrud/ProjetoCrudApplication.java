b83d25c867856f7579824c43c253d7b39d16b212
psouza0 <paulogeraldols@gmail.com>
2023-11-04 22:54:04 -0300

    add projeto ao github

---
+++ src/main/java/com/pgls/projetocrud/ProjetoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pgls.projetocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgls/projetocrud/ProjetoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgls.projetocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
